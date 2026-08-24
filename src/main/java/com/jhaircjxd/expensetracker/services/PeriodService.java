f160cffd72ab84ff849581129fd8950b7901e6a6
JhairCJ <jhaircjxd@gmail.com>
2026-08-24 14:17:59 -0400

    feat: implement PeriodService methods for create, update, delete, and retrieve periods

--- src/main/java/com/jhaircjxd/expensetracker/services/PeriodService.java
+++ src/main/java/com/jhaircjxd/expensetracker/services/PeriodService.java
@@ -5,26 +5,42 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.jhaircjxd.expensetracker.entities.Period;
+import com.jhaircjxd.expensetracker.repositories.PeriodRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PeriodService {
+
+    private final PeriodRepository periodRepository;
+
     public Period createPeriod(Period period){
-        return null;
+        return periodRepository.save(period);
     }
 
     public Period updatePeriod(Long id, Period period){
-        return null;
+        Period existingPeriod = periodRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Period not found"));
+        
+        existingPeriod.setEarnings(period.getEarnings());
+        existingPeriod.setExpenses(period.getExpenses());
+        existingPeriod.setSavings(period.getSavings());
+        existingPeriod.setTransactions(period.getTransactions());
+
+        return periodRepository.save(existingPeriod);
     }
 
     public void deletePeriod(Long id){
-
+        periodRepository.deleteById(id);
     }
 
     public List<Period> getPeriods(){
-        return null;
+        return periodRepository.findAll();
     }
 
     public Period getPeriodById(Long id){
-        return null;
+        return periodRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Period not found with id: " + id));
     }
 }
