577c47f6638d12e200c22ff62bdfbad0e4267840
JhairCJ <jhaircjxd@gmail.com>
2026-08-24 14:24:18 -0400

    feat: implement create, update, and delete methods in CategoryPeriodService

--- src/main/java/com/jhaircjxd/expensetracker/services/CategoryPeriodService.java
+++ src/main/java/com/jhaircjxd/expensetracker/services/CategoryPeriodService.java
@@ -5,19 +5,32 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.jhaircjxd.expensetracker.entities.CategoryPeriod;
+import com.jhaircjxd.expensetracker.repositories.CategoryPeriodRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryPeriodService {
 
+    private final CategoryPeriodRepository categoryPeriodRepository;
+
     public CategoryPeriod createCategoryPeriod(CategoryPeriod categoryPeriod){
-        return null;
+        return categoryPeriodRepository.save(categoryPeriod);
     }
 
     public CategoryPeriod updateCategoryPeriod(Long id, CategoryPeriod categoryPeriod){
-        return null;
+        CategoryPeriod existingCategoryPeriod = categoryPeriodRepository.findById(id)
+                            .orElseThrow(() -> new RuntimeException("Category Period not found"));
+
+        existingCategoryPeriod.setBudget(categoryPeriod.getBudget());
+        existingCategoryPeriod.setSpent(categoryPeriod.getSpent());
+
+        return categoryPeriodRepository.save(existingCategoryPeriod);
     }
 
     public void deleteCategoryPeriod(Long id){
+        categoryPeriodRepository.deleteById(id);
     }
 
     //Returns category periods using a period_id
